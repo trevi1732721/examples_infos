ba016243a945a9b1517230154a50d330de19cfb4
trevi1732721 <vincent2000.tremblay@gmail.com>
2024-03-21 20:28:40 -0400

    add the tabs

---
+++ Tableaux.py
@@ -0,0 +1,35 @@
+#tableau 1D (liste)
+tab1d = [4,5,6]
+
+# Voici comment acceder aux elements du tableau
+# (examples avec le deuxieme element du tableau)
+print(tab1d[1])
+
+# Voici comment modifier un element du tableau
+# (examples avec le troisieme element du tableau)
+tab1d[2] = 8
+print(tab1d)
+
+
+#tableau 2D (grille)
+tab2d = [[4,5,6],[8,9,10]]
+
+# Voici comment acceder aux elements du tableau
+# (examples avec l'element de la deuxieme ligne troisieme colone)
+print(tab2d[1][2])
+
+# Voici comment modifier un element du tableau
+# (examples avec le troisieme element de la premiere ligne)
+tab2d[0][2] = 12
+print(tab2d)
+
+# Voici comment acceder aux elements du tableau
+# (examples avec l'element de la deuxieme ligne troisieme colone)
+print(tab2d[1][2])
+
+'''
+Il est possible de faire des tableaux de taille n, il suffit de suivre la meme logique pour acceder aux valeurs
+
+Vincent Tremblay, mars 2024
+'''
+
